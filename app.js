a8d95cf7c28c2446bcbb525b874a784bb5f14d14
willin <willin@willin.willin>
2022-05-29 16:27:09 -0500

    Estructura y comunicacion de elementos en el back

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var cliente_route = require('./routes/cliente');
 var port = process.env.PORT || 4201;
 
 var app = express();
@@ -15,6 +16,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/proyecto',(err, res) => {
             console.log('servidor corriendo en el puerto ' + port);
         });
     }
-})
+});
 
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json({limit: '50mb' ,extended: true}));
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/api', cliente_route);
 module.exports = app;
